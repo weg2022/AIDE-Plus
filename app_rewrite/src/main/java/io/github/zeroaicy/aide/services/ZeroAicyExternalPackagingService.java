d838ceefc7a53056c2dfeb9aa2509563ffa46279
ZeroAicy <ZeroAicy@qq.com>
2024-04-17 13:23:31 +0800

    打包进程 初始化App

--- app_rewrite/src/main/java/io/github/zeroaicy/aide/services/ZeroAicyExternalPackagingService.java
+++ app_rewrite/src/main/java/io/github/zeroaicy/aide/services/ZeroAicyExternalPackagingService.java
@@ -1,11 +1,15 @@
 package io.github.zeroaicy.aide.services;
 import com.aide.ui.build.packagingservice.ExternalPackagingService;
 import io.github.zeroaicy.util.Log;
+import com.aide.ui.App;
 
 public class ZeroAicyExternalPackagingService extends ExternalPackagingService {
 	@Override
 	public void onCreate() {
 		try {
+			// 初始化 App
+			App.sh(getApplicationContext());
+			
 			ExternalPackagingService.ExternalPackagingServiceWorker externalPackagingServiceWorker = getExternalPackagingServiceWorker();
 			if (externalPackagingServiceWorker != null) {
 				//释放旧的
