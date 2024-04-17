6d5d66284fb7b87beb16247bfec1d28bdef73cfe
ZeroAicy <ZeroAicy@qq.com>
2024-04-17 13:24:08 +0800

    优化全局异常捕获显示

--- app_rewrite/src/main/java/io/github/zeroaicy/ZeroAicyAIDEApplication.java
+++ app_rewrite/src/main/java/io/github/zeroaicy/ZeroAicyAIDEApplication.java
@@ -23,45 +23,43 @@ public class ZeroAicyAIDEApplication extends AIDEApplication {
 		ShizukuProvider.enableMultiProcessSupport(true);
 	}
 	@Override
-	public void onCreate(){
+	public void onCreate() {
 		super.onCreate();
-		
-		DebugUtil.debug();
-		
-		CrashApphandler.getInstance().onCreated();
-		
 		//解除反射
 		Log.d(TAG, "解除反射: " + ReflectPie.reflectAll());
 		
-		//初始化ZeroAicy设置
-		ZeroAicySetting.init(this);
-		if( ContextUtil.isMainProcess()){
-			//初始化Shizuku库
-			ShizukuUtil.initialized(this);
+		String crashProcessName = getPackageName() + ".crash";
+		String curProcessName = ContextUtil.getProcessName();
+		if (crashProcessName.equals(curProcessName)) {
+			return;
 		}
 		
-		Log.d(TAG, "Application初始化耗时: " + (System.currentTimeMillis() - now) + "ms");
+		DebugUtil.debug();
+		CrashApphandler.getInstance().onCreated();
 
-		//testLog();
+		//初始化ZeroAicy设置
+		ZeroAicySetting.init(this);
+		
+		Log.d(TAG, "Application初始化耗时: " + (System.currentTimeMillis() - now) + "ms");
 	}
-	
+
 	/**
 	 * 当日志系统崩溃是进行修复，以便测试
 	 * 依赖反射注意检查
 	 */
 	private void testLog() {
 		boolean log = Log.getLog() == null;
-		
-		if( log ){
+
+		if (log) {
 			Log.AsyncOutputStreamHold logHold = Log.getLogHold();
 			String logCatPath = FileUtil.LogCatPath + "_test.txt";
 
-			if( logHold == null ){
+			if (logHold == null) {
 				Log.d(TAG, "LogHold 为 null");
 				logHold = new Log.AsyncOutputStreamHold(logCatPath);
 				ReflectPie.on(Log.class).set("mLogHold", logHold);
 			}
-			if( log = Log.getLog() == null){
+			if (log = Log.getLog() == null) {
 				Log.d(TAG, "LogHold mLog null");
 				FileOutputStream createOutStream = Log.AsyncOutputStreamHold.createOutStream(logCatPath);
 				Log.AsyncOutputStreamHold.AsyncOutStream asyncOutStream = new Log.AsyncOutputStreamHold.AsyncOutStream(createOutStream);
@@ -72,9 +70,9 @@ public class ZeroAicyAIDEApplication extends AIDEApplication {
 			ReflectPie.on(Log.class).call("printPreMsgList");
 		}
 	}
-	
+
 	static{
-		
+
 		//init();
 	}
 
@@ -87,8 +85,8 @@ public class ZeroAicyAIDEApplication extends AIDEApplication {
 
 		apkClassLoaderReflectPie.set("parent", new ProxyClassLoader2(parent));
 	}
-	public static class ProxyClassLoader2 extends ClassLoader{
-		public ProxyClassLoader2(ClassLoader classLoader){
+	public static class ProxyClassLoader2 extends ClassLoader {
+		public ProxyClassLoader2(ClassLoader classLoader) {
 			super(classLoader);
 		}
 
@@ -97,7 +95,7 @@ public class ZeroAicyAIDEApplication extends AIDEApplication {
 			Log.d("ProxyClassLoader2", "加载: " + name);
 			return super.loadClass(name, resolve);
 		}
-		
-		
+
+
 	}
 }
